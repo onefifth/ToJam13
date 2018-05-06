c0e93591a3be247b3f4b061cac986193903a0961
davidrusak <davidrusak@gmail.com>
2018-05-06 13:59:47 -0400

    buncha npc work

---
+++ Assets/Scripts/PickARandomSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickARandomSprite : MonoBehaviour {
+
+    public List<Sprite> sprites;
+
+	private void Start()
+	{
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Count)];
+	}
+}
